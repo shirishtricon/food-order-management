1becbd0a65bcaceb74879b847c6f0d89c71684f2
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-03-09 18:15:08 +0530

    added category component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './View/navbar/navbar.component';
+import { CategoriesComponent } from './View/categories/categories.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoriesComponent,
+
   ],
   imports: [
     BrowserModule,
