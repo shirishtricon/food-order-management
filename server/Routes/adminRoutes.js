67c969f28bfc84e54245c979002cdc11fed8f3aa
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-04-12 15:09:02 +0530

    feat: verify token from the headers

--- server/Routes/adminRoutes.js
+++ server/Routes/adminRoutes.js
@@ -5,8 +5,10 @@ const cors = require("cors");
 const categoryController = require("../Controllers/category.controller");
 const itemController = require("../Controllers/item.controller");
 const adminController = require("../Controllers/admin.controller");
+const userController = require("../Controllers/user.controller");
 const multer = require("multer");
 const upload = multer();
+const verifyToken = require("../Middleware/verifyToken");
 
 app.use(
   cors({
@@ -15,16 +17,28 @@ app.use(
 );
 
 // APIs for categories
-router.get("/categories", categoryController.getAllCategories);
-router.post("/category", categoryController.addCategory);
-router.put("/category/:uuid", categoryController.updateCategory);
-router.delete("/category/:uuid", categoryController.deleteCategory);
+router.get(
+  "/categories",
+  verifyToken("Admin", "User"),
+  categoryController.getAllCategories
+);
+router.post("/category", verifyToken("Admin"), categoryController.addCategory);
+router.put(
+  "/category/:uuid",
+  verifyToken("Admin"),
+  categoryController.updateCategory
+);
+router.delete(
+  "/category/:uuid",
+  verifyToken("Admin"),
+  categoryController.deleteCategory
+);
 
 //APIs for Items
-router.get("/items", itemController.getAllItems);
-router.post("/item", itemController.addItem);
-router.put("/item/:uuid", itemController.updateItem);
-router.delete("/item/:uuid", itemController.deleteItem);
+router.get("/items", verifyToken("Admin", "User"), itemController.getAllItems);
+router.post("/item", verifyToken("Admin"), itemController.addItem);
+router.put("/item/:uuid", verifyToken("Admin"), itemController.updateItem);
+router.delete("/item/:uuid", verifyToken("Admin"), itemController.deleteItem);
 
 //APIs for Admin
 router.get("/getAllAdmins", adminController.getAllAdmin);
@@ -33,12 +47,17 @@ router.put("/updateAdmin/:uuid", adminController.updateAdmin);
 router.delete("/deleteAdmin/:uuid", adminController.deleteAdmin);
 
 //APIs for users
-// router.get("/users", userController.getAllUsers);
-// router.post("/user", userController.addUser);
-// router.put("/user/:id", userController.updateUser);
-// router.delete("/user/:id", userController.deleteUSer);
+router.get("/users", verifyToken("Admin"), userController.getAllUsers);
+router.post("/user", verifyToken("Admin"), userController.addUser);
+router.put("/user/:id", verifyToken("Admin"), userController.updateUser);
+router.delete("/user/:id", verifyToken("Admin"), userController.deleteUSer);
 
 //API for Bulk write
-router.post("/upload", upload.single("file"), itemController.adddBulkItems);
+router.post(
+  "/upload",
+  upload.single("file"),
+  verifyToken("Admin"),
+  itemController.adddBulkItems
+);
 
 module.exports = router;
