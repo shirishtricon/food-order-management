9ca9f8a59e5b74dde0f03cf92abab0bc6ec21e25
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-04-12 15:14:46 +0530

    feat: Add bulk items upload functionality

---
+++ client/src/app/View/bulk-upload/bulk-upload.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BulkUploadService } from 'src/app/Model/Services/item/bulk-upload.service';
+
+declare var window: any;
+
+@Component({
+  selector: 'app-bulk-upload',
+  templateUrl: './bulk-upload.component.html',
+  styleUrls: ['./bulk-upload.component.css']
+})
+export class BulkUploadComponent implements OnInit{
+  file: File | null = null;
+  errorMessage: string = '';
+  @ViewChild('bulkItemModal', { static: false }) bulkItemModal: any;
+  formModal: any;
+
+  constructor(private bulkUploadService: BulkUploadService) { }
+
+  ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('bulkItemModal')
+    )
+  }
+
+  onFileSelected(event: any): void {
+    let fileType = event.target.files[0].name.substr( event.target.files[0].name.lastIndexOf('.') + 1);
+    if(fileType === 'xlsx' || fileType === 'csv') {
+      this.file = event.target.files[0];
+    } else {
+      this.errorMessage = 'Invalid File type'
+    }
+    
+  }
+
+  onUpload(): void {
+    if (!this.file) {
+      this.errorMessage = 'Please select a file'
+      console.error('No file selected.');
+      return;
+    }
+
+    this.bulkUploadService.uploadFile(this.file).subscribe(
+      (response) => {
+        console.log('File uploaded successfully.');
+        this.errorMessage = '';
+        this.openModal();
+      },
+      (error) => {
+        console.error('File upload failed:', error);
+        this.errorMessage = 'Invalid file';
+      }
+    );
+  }
+
+  openModal() {
+    this.formModal.show();
+  }
+}
