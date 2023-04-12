73839a445b61388671dcb1540fe206902d6a59e2
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-04-12 15:09:47 +0530

    feat: verify token from the headers

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const cors = require("cors");
 const userController = require("../Controllers/user.controller");
+const verifyToken = require("../Middleware/verifyToken");
 
 app.use(
   cors({
@@ -11,9 +12,9 @@ app.use(
 );
 
 //APIs for users
-router.get("/", userController.getAllUsers);
+router.get("/", verifyToken("Admin"), userController.getAllUsers);
 router.post("/", userController.addUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUSer);
+router.put("/:id", verifyToken("Admin", "User"), userController.updateUser);
+router.delete("/:id", verifyToken("Admin", "User"), userController.deleteUSer);
 
 module.exports = router;
