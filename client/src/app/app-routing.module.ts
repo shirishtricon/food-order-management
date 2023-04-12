36820a8d9e95dd8887729120a3afbed6720a087c
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-04-12 14:53:33 +0530

    refactor: modify routes to add children for Admin and User

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,14 +10,25 @@ import { LoginComponent } from './View/login/login.component';
 import { UserComponent } from './View/user/user.component';
 import { AuthGuard } from './_auth/auth.guard';
 
+
 const routes: Routes = [
-  { path: 'categories', component: CategoriesComponent },
+  // { path: 'categories', component: CategoriesComponent },
   { path: '', component: HomeComponent },
-  { path: 'addItem', component: AddItemComponent },
-  { path: 'addCategory', component: AddCategoryComponent },
+  // { path: 'addItem', component: AddItemComponent, canActivate:[AuthGuard], data:{roles:['Admin']} },
+  // { path: 'addCategory', component: AddCategoryComponent, canActivate:[AuthGuard], data:{roles:['Admin']} },
   { path: 'home', component: HomeComponent},
-  { path: 'admin', component: AdminComponent, canActivate:[AuthGuard], data:{roles:['Admin']}},
-  { path: 'user', component: UserComponent, canActivate:[AuthGuard], data:{roles:['User']}},
+  { path: 'admin', component: AdminComponent, canActivate:[AuthGuard], data:{roles:['Admin']},
+    children: [
+      {path: 'categories', component: CategoriesComponent},
+      {path: 'addItem', component: AddItemComponent}, 
+      {path: 'addCategory', component: AddCategoryComponent}
+    ]
+},
+  { path: 'user', component: UserComponent, canActivate:[AuthGuard], data:{roles:['User']},
+  children: [
+    {path: 'categories', component: CategoriesComponent}
+  ]
+},
   { path: 'forbidden', component: ForbiddenComponent}
 ];
 
