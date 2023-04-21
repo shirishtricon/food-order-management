88a421c2acc407ef9d6ad0df83efeae5a4988a5c
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-04-21 12:00:04 +0530

    feat: Add Orders model

---
+++ server/models/orders.model.js
@@ -0,0 +1,36 @@
+const { Sequelize } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  const orders = sequelize.define("orders", {
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+
+    user_uuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "uuid",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    },
+    items: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+
+    subtotal: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+
+      allowNull: false,
+    },
+  });
+  return orders;
+};
