eb31adf38956527a5e4a2fb844088f67a96f4ff8
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-04-24 18:27:51 +0530

    feat: implement toast service

---
+++ client/src/app/Model/Services/order/order.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Order } from '../../order.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(private http: HttpClient) { }
+
+  addOrder(order: Order) {
+    return this.http.post<Order[]>('http://localhost:5000/user/order',order)
+  }
+}
