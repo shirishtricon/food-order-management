11a8aaabbbdc4e20e8814d9e8a264bedcaa3c81f
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-04-24 18:20:05 +0530

    feat: update order if order for the day exist

--- server/Services/order.service.js
+++ server/Services/order.service.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const db = require("../models");
 
 const Orders = db.sequelize.models.orders;
@@ -5,6 +6,17 @@ const Orders = db.sequelize.models.orders;
 const getAllOrders = async () => {
   const data = await Orders.findAll();
   return data;
+};
+
+const getSingleOrderByUuidAndDate = async (user_uuid, date) => {
+  try {
+    let data = await Orders.findOne({
+      where: { user_uuid: user_uuid, date: date },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const addOrder = async (orderDetails) => {
@@ -17,7 +29,21 @@ const addOrder = async (orderDetails) => {
     });
 };
 
+const updateOrder = async (user_uuid, details) => {
+  await Orders.update(details, {
+    where: { user_uuid: user_uuid },
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 module.exports = {
   addOrder,
+  getSingleOrderByUuidAndDate,
   getAllOrders,
+  updateOrder,
 };
