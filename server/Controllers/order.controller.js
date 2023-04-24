b30d6c6f5e69a2b698101c396fce0b8c90d450c7
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-04-24 17:56:37 +0530

    feat: Update order feature if the order already exist for the day

--- server/Controllers/order.controller.js
+++ server/Controllers/order.controller.js
@@ -1,5 +1,6 @@
 const services = require("../Services");
 const orderService = services.orderService;
+const jwt = require("jsonwebtoken");
 
 const getAllOrders = async (req, res) => {
   try {
@@ -13,12 +14,34 @@ const getAllOrders = async (req, res) => {
 const addOrder = async (req, res) => {
   try {
     let orderDetails = req.body;
+    let token = req.headers.token;
+    let user_uuid = jwt.decode(token).uuid;
     let items = JSON.stringify(req.body.items);
     let date = new Date();
     orderDetails.items = items;
     orderDetails.date = date;
-    console.log(orderDetails);
-    await orderService.addOrder(orderDetails);
+    orderDetails.user_uuid = user_uuid;
+
+    //check if order exist for a particular date
+    const currentDate = new Date().toISOString().slice(0, 10);
+    const existingRecord = await orderService.getSingleOrderByUuidAndDate(
+      user_uuid,
+      currentDate
+    );
+    if (existingRecord) {
+      const existingItems = JSON.parse(existingRecord.items);
+      const updatedItems = existingItems.concat(JSON.parse(req.body.items));
+      console.log(typeof req.body.items);
+      delete orderDetails.user_uuid;
+      orderDetails.date = currentDate;
+      orderDetails.subtotal = +existingRecord.subtotal + +req.body.subtotal;
+      orderDetails.items = JSON.stringify(updatedItems);
+      console.log(orderDetails);
+      data = await orderService.updateOrder(user_uuid, orderDetails);
+    } else {
+      await orderService.addOrder(orderDetails);
+    }
+
     res.status(200).json({ message: "Order added successfully" });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
