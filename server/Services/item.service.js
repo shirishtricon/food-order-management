b9541274e4a4b18e4bff78755ca714f463338be9
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-02 12:14:49 +0530

    chore: add comments for addItem API

--- server/Services/item.service.js
+++ server/Services/item.service.js
@@ -4,7 +4,7 @@ const Items = db.sequelize.models.items;
 
 const getAllItems = async () => {
   const data = await Items.findAll({
-    attributes: ["uuid", "id", "name", "price", "category_id"],
+    attributes: ["uuid", "name", "price", "category_uuid"],
   });
   return data;
 };
@@ -19,22 +19,26 @@ const getItemByName = async (name) => {
 };
 
 const addItem = async (ItemDetails) => {
-  await Items.create(ItemDetails)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  const data = await Items.findOne({
-    order: [["id", "DESC"]],
-    attributes: ["id", "name"],
-  });
+  const data = await Items.create(ItemDetails);
+  // .then((res) => {
+  //   return res;
+  // })
+  // .catch((err) => {
+  //   throw new Error(err);
+  //   console.log(err);
+  // });
   return data;
 };
 
 const adddBulkItems = async (bulkData) => {
-  await Items.bulkCreate(bulkData, { validate: true });
+  await Items.bulkCreate(bulkData, { validate: true })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      throw new error(err);
+      console.log(err);
+    });
 };
 
 const updateItem = async (uuid, body) => {
