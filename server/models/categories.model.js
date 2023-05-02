9d08db0e5dd02071be06f37283f450f26af88556
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-02 12:20:01 +0530

    refactor: remvoe id column

--- server/models/categories.model.js
+++ server/models/categories.model.js
@@ -1,26 +1,21 @@
-
 module.exports = (sequelize, DataTypes) => {
-    const categories = sequelize.define('categories', {
-        uuid: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true 
-        },
-
-        id: {
-            unique: true,
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false
-        },
-
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-    },         
+  const categories = sequelize.define(
+    "categories",
     {
-        timestamps: false
-    });
-    return categories;
-}
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+  return categories;
+};
