9f55ac7924e064ba396eca2c49d1acbf265a914b
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-02 11:29:07 +0530

    feat: get single user order by user uuid

--- client/src/app/Model/Services/order/order.service.ts
+++ client/src/app/Model/Services/order/order.service.ts
@@ -12,4 +12,8 @@ export class OrderService {
   addOrder(order: Order) {
     return this.http.post<Order[]>('http://localhost:5000/user/order',order)
   }
+
+  getUserOrder(uuid: string) {
+    return this.http.get<any>(`http://localhost:5000/user/order/${uuid}`)
+  }
 }
