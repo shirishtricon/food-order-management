0ba95a1b7a9f79c84166589a8b27a2ff8a8bfe45
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-05 12:57:41 +0530

    refactor: change id to uuid

--- server/Utils/changeCategoryNameToId.js
+++ server/Utils/changeCategoryNameToId.js
@@ -1,15 +1,15 @@
 const categoryService = require("../Services/category.service");
 
 const categoryNameToUuid = async (validCategoryData) => {
-  let validCategoryWithIdData = [];
+  let validCategoryWithUuidData = [];
 
   for (item of validCategoryData) {
     const category = await categoryService.getCategoryByName(item.category);
     delete item.category;
     item.category_uuid = category.uuid;
-    validCategoryWithIdData.push(item);
+    validCategoryWitUuidData.push(item);
   }
-  return validCategoryWithIdData;
+  return validCategoryWithUuidData;
 };
 
 module.exports = { categoryNameToUuid };
