091656053421ef0e155d4ff9bf249289b3fefcb9
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-18 11:06:29 +0530

    test: write test for data service

--- client/src/app/Model/Services/data.service.spec.ts
+++ client/src/app/Model/Services/data.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { DataService } from './data.service';
 
 describe('DataService', () => {
@@ -12,5 +11,17 @@ describe('DataService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set and get total price', () => {
+    const price = 100;
+    service.setTotalPrice(price);
+    expect(service.getTotalPrice()).toEqual(price);
+  });
+
+  it('should set and get role', () => {
+    const role = 'admin';
+    service.setRole(role);
+    expect(service['role']).toEqual(role); 
   });
 });
