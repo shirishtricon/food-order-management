f5d8e67c63cf0c4c77d64f45224af473c643092c
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-18 12:36:43 +0530

    test: write test cases for user component

--- client/src/app/View/user/user.component.spec.ts
+++ client/src/app/View/user/user.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
@@ -8,6 +8,7 @@ describe('UserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
       declarations: [ UserComponent ]
     })
     .compileComponents();
