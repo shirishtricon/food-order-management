aa166d676bd6b56461c9074706bf832b953c2718
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-18 11:12:10 +0530

    test: write test cases for toast service

--- client/src/app/Model/Services/toast.service.spec.ts
+++ client/src/app/Model/Services/toast.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ToastService } from './toast.service';
 
 describe('ToastService', () => {
@@ -12,5 +11,24 @@ describe('ToastService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a toast', () => {
+    service.show('Hello, World!');
+    expect(service.toasts.length).toBe(1);
+    expect(service.toasts[0].textOrTpl).toBe('Hello, World!');
+  });
+
+  it('should remove a toast', () => {
+    const toast = { textOrTpl: 'Hello, World!' };
+    service.toasts.push(toast);
+    service.remove(toast);
+    expect(service.toasts.length).toBe(0);
+  });
+
+  it('should clear all toasts', () => {
+    service.toasts.push({ textOrTpl: 'Toast 1' }, { textOrTpl: 'Toast 2' }, { textOrTpl: 'Toast 3' });
+    service.clear();
+    expect(service.toasts.length).toBe(0);
   });
 });
