3b7435d83d339bffacc5ab7117edf3d419518e25
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-18 12:23:57 +0530

    refactor: change modal initialization in ngOnInit method

--- client/src/app/View/login/login.component.ts
+++ client/src/app/View/login/login.component.ts
@@ -1,70 +1,71 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
-import { LoginService } from 'src/app/Model/Services/login/login.service';
-import { UserAuthService } from 'src/app/Model/Services/user-auth.service';
-import { UserService } from 'src/app/Model/Services/user/user.service';
+  import { Component, OnInit, ViewChild } from '@angular/core';
+  import { NgForm } from '@angular/forms';
+  import { Router } from '@angular/router';
+  import { NgxUiLoaderService } from 'ngx-ui-loader';
+  import { LoginService } from 'src/app/Model/Services/login/login.service';
+  import { UserAuthService } from 'src/app/Model/Services/user-auth.service';
+  import { UserService } from 'src/app/Model/Services/user/user.service';
 
-declare var window: any;
+  declare var window: any;
+  import { Modal } from 'bootstrap';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent implements OnInit{
-  @ViewChild('loginForm') form = NgForm;
-  @ViewChild('loginModal', { static: false }) loginModal: any;
-  message: any;
-  login: boolean = false;
-  formModal: any;
+  @Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+  })
+  export class LoginComponent implements OnInit{
+    @ViewChild('loginForm') form = NgForm;
+    @ViewChild('loginModal', { static: false }) loginModal: any;
+    message: any;
+    login: boolean = false;
+    formModal: any;
 
-  ngOnInit(): void {
-    this.formModal = new window.bootstrap.Modal(
-      document.getElementById('loginModal')
-    )
-  }
+    ngOnInit(): void {
+      this.formModal = new Modal(
+        document.getElementById('loginModal') as HTMLElement
+      );
+    }
 
-  constructor(private loginServie: LoginService, 
-              private userAuthService: UserAuthService,
-              private router: Router,
-              private ngxService: NgxUiLoaderService) { }
+    constructor(private loginServie: LoginService, 
+                private userAuthService: UserAuthService,
+                private router: Router,
+                private ngxService: NgxUiLoaderService) { }
 
-  onLogin(form:any) {
-    if(form.valid) {
-      this.loginServie.login(form.value).subscribe((response) => {
-        this.ngxService.start();
-        console.log(response);
-        this.login = true;
-        this.userAuthService.setToken(response.token);
-        const tokenDetails = this.userAuthService.decodedToken();
-        console.log(tokenDetails);
-        
-        setTimeout(() => {
-         
-          this.ngxService.stop();
-          if(tokenDetails.role === 'Admin') {
-            this.router.navigate(['/admin/categories'])
-          } else {
-            this.router.navigate(['/user/categories'])
+    onLogin(form:any) {
+      if(form.valid) {
+        this.loginServie.login(form.value).subscribe((response) => {
+          this.ngxService.start();
+          console.log(response);
+          this.login = true;
+          this.userAuthService.setToken(response.token);
+          const tokenDetails = this.userAuthService.decodedToken();
+          console.log(tokenDetails);
+          
+          setTimeout(() => {
+          
+            this.ngxService.stop();
+            if(tokenDetails.role === 'Admin') {
+              this.router.navigate(['/admin/categories'])
+            } else {
+              this.router.navigate(['/user/categories'])
+            }
+          },2000)
+          
+        }, (error) => {
+          console.log(error);
+          if(error.error) {
+            this.message = error.error.message;
+            this.openModal();
           }
-        },2000)
-        
-      }, (error) => {
-        console.log(error);
-        if(error.error) {
-          this.message = error.error.message;
-          this.openModal();
-        }
-      })
-    } else {
-      this.message = "Enter All Details"
-      this.openModal();
+        })
+      } else {
+        this.message = "Enter All Details"
+        this.openModal();
+      }
+    }
+
+    openModal() {
+      this.formModal.show();
     }
   }
-
-  openModal() {
-    this.formModal.show();
-  }
-}
