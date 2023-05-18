fd6637ea987ae24c79986bfdb8eb1de013e56bed
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-18 11:09:50 +0530

    test: write test cases for login service

--- client/src/app/Model/Services/login/login.service.spec.ts
+++ client/src/app/Model/Services/login/login.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { LoginService } from './login.service';
 
 describe('LoginService', () => {
   let service: LoginService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ LoginService ]
+    });
     service = TestBed.inject(LoginService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('login', () => {
+    it('should send a POST request to the server', () => {
+      const mockLoginData = { username: 'test', password: 'password' };
+      const expectedResponse = { token: 'abc123' };
+      service.login(mockLoginData).subscribe(response => {
+        expect(response).toEqual(expectedResponse);
+      });
+      const request = httpMock.expectOne('http://localhost:5000/login');
+      expect(request.request.method).toBe('POST');
+      expect(request.request.body).toEqual(mockLoginData);
+      request.flush(expectedResponse);
+    });
   });
 });
