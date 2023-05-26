8d7ef26b53048ed22ba9983cc149d91f591cb0a6
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-26 12:35:49 +0530

    refactor: replace hardcoded URL with API_BASE_URL

--- client/src/app/Model/Services/category/category.service.ts
+++ client/src/app/Model/Services/category/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
 import { Category } from '../../category.model';
+import { API_BASE_URL } from '../../../../api.config'
 
 
 @Injectable({
@@ -11,10 +12,10 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   getCategories() {
-   return this.http.get<Category[]>('http://localhost:5000/admin/categories') 
+   return this.http.get<Category[]>(`${API_BASE_URL}/admin/categories`) 
   }
 
   addCategory(category: Category) {
-    return this.http.post<Category[]>('http://localhost:5000/admin/category',category)
+    return this.http.post<Category[]>(`${API_BASE_URL}/admin/category`,category)
   }
 }
