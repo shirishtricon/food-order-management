0cff8c3580de848855e53f8a7b50ae085ca78b0d
Shirish Kulkarni <shirish.kulkarn@triconinfotech.com>
2023-05-29 12:46:35 +0530

    refactor: update API URL with address of EC2 server

--- client/src/app/Model/Services/order/order.service.ts
+++ client/src/app/Model/Services/order/order.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Order } from '../../order.model';
+import { API_BASE_URL } from '../../../../api.config'
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,10 @@ export class OrderService {
   constructor(private http: HttpClient) { }
 
   addOrder(order: Order) {
-    return this.http.post<Order[]>('http://localhost:5000/user/order',order)
+    return this.http.post<Order[]>(`${API_BASE_URL}/user/order`,order)
   }
 
   getUserOrder(uuid: string, fromDate: string, toDate: string) {
-    return this.http.get<any>(`http://localhost:5000/user/order/${uuid}`, {params: {fromDate: fromDate, toDate: toDate}})
+    return this.http.get<any>(`${API_BASE_URL}/user/order/${uuid}`, {params: {fromDate: fromDate, toDate: toDate}})
   }
 }
